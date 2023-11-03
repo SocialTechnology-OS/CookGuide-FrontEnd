d3023fdd0eb1857da7a23972b3ed938ddcf72ec4
William062004 <U202117904@upc.edu.pe>
2023-11-02 22:50:18 -0500

    REGISTER AND LOGIN
    LIAM

--- principals-cookguide/src/app/app.module.ts
+++ principals-cookguide/src/app/app.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from "@angular/material/table";
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../shared/material.module';
 import { RouterModule } from '@angular/router';
 import { FrontCardsComponent } from './front/components/front-cards/front-cards.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
 import { ManageRecipesComponent } from './front/components/manage-recipes/manage-recipes.component';
 import { FormCreateRecipesComponent } from './front/components/form-create-recipes/form-create-recipes.component';
@@ -18,17 +42,25 @@ import { SidenavComponent } from './public/components/sidenav/sidenav.component'
 import { BodyComponent } from './public/components/body/body.component';
 import { FromUpdateRecipesComponent } from './front/components/from-update-recipes/from-update-recipes.component';
 import { RecipesDetailsComponent } from './front/components/recipes-details/recipes-details.component';
+import { LoginComponent } from './front/components/login/login.component';
+import { RegisterComponent } from './front/components/register/register.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontCardsComponent, 
-    SidenavComponent, 
+    FrontCardsComponent,
+    SidenavComponent,
     BodyComponent,
     ManageRecipesComponent,
     FormCreateRecipesComponent,
     FromUpdateRecipesComponent,
     RecipesDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+
 
   ],
   imports: [
@@ -39,7 +71,29 @@ import { RecipesDetailsComponent } from './front/components/recipes-details/reci
     RouterModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatProgressBarModule,
+    MatNativeDateModule,
+    MatListModule,
+    MatMenuModule,
+    MatStepperModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- principals-cookguide/src/app/services/user-service/user-service.service.ts
+++ principals-cookguide/src/app/services/user-service/user-service.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { User } from '../../models/user.model';
+import {Subject} from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +11,31 @@ import { User } from '../../models/user.model';
 export class UserServiceService {
   constructor(private http: HttpClient) {}
 
+  private userChangedSubject = new Subject<void>();
+
+  private onUserChangedSubject(){
+    this.userChangedSubject.next();
+  }
+  userChanged$=this.userChangedSubject.asObservable();
+
 
   getUserList() {
     return this.http
       .get<User[]>(`${environment.baseURL}/users`)
   }
-  
-  
+
+
   getUserById(id: number) {
     return this.http
       .get<User>(`${environment.baseURL}/users/${id}`)
   }
 
-     
+  createUser(user : User){
+    return this.http.
+        post(`${environment.baseURL}/users`, user).pipe(tap(()=>{
+          this.onUserChangedSubject();
+    }))
+  }
+
+
 }
